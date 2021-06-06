6af7488876f95803f4076fd8c277927c28c26699
Aries Cs <aries0d0f@gmail.com>
2021-06-06 18:43:12 +0800

    fix: GLFW should forward OpenGL compat on macOS.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-int main() {
+int main()
+{
   // Initialize GLFW
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // GLFW OpenGL Forward for macOS
 
   return 0;
 }
