45f5fb3678111cd6ac0ae912d7ba8e6239abc8c2
Aries Cs <aries0d0f@gmail.com>
2021-06-06 18:44:08 +0800

    feat: Impl create window and check GLFW & GLAD.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+  glViewport(0, 0, width, height);
+}
 
 int main()
 {
@@ -9,6 +15,35 @@ int main()
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // GLFW OpenGL Forward for macOS
+
+  // Create Window
+  GLFWwindow *window = glfwCreateWindow(800, 640, "GLSL-Practice", NULL, NULL);
+
+  // Check GLFW
+  if (window == NULL)
+  {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+
+  // Check GLAD
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+  {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  while (!glfwWindowShouldClose(window))
+  {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
 
   return 0;
 }
