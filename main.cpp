3be98bd5f20fdecd9a760c8ebd4022770924db98
Aries Cs <aries0d0f@gmail.com>
2021-06-06 19:22:57 +0800

    feat: Use shader loader.

--- main.cpp
+++ main.cpp
@@ -1,17 +1,15 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
 #include <iostream>
 
-void framebuffer_size_callback(GLFWwindow *window, int width, int height)
-{
-  glViewport(0, 0, width, height);
-}
+#include "lib/shader.hpp"
 
-void processInput(GLFWwindow *window)
-{
-  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-    glfwSetWindowShouldClose(window, true);
-}
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void processInput(GLFWwindow *window);
+
+const unsigned int WIDTH = 800;
+const unsigned int HEIGHT = 640;
 
 int main()
 {
@@ -20,7 +18,10 @@ int main()
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // GLFW OpenGL Forward for macOS
+#endif
 
   // Create Window
   GLFWwindow *window = glfwCreateWindow(800, 640, "GLSL-Practice", NULL, NULL);
@@ -33,6 +34,7 @@ int main()
     return -1;
   }
   glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
   // Check GLAD
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
@@ -41,24 +43,73 @@ int main()
     return -1;
   }
 
-  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+  // Load shaders
+  Shader shader("shaders/main.vert", "shaders/main.frag");
+
+  float vertices[] = {
+    /**
+     * --------------------------------------
+     * | Positions        | Colors          |
+     * --------------------------------------
+     */
+        0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
+       -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
+        0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
+  };
+
+  unsigned int VertexBufferObject, VertexArrayObject;
+  glGenVertexArrays(1, &VertexArrayObject);
+  glGenBuffers(1, &VertexBufferObject);
+  // Bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+  glBindVertexArray(VertexArrayObject);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VertexBufferObject);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  // Position attribute
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+  // Color attribute
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
 
   // Renderer
   while (!glfwWindowShouldClose(window))
   {
     // Input
     processInput(window);
-    
+
     // Clear Buffer
     glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    // Render from Shader
+    shader.use();
+    glBindVertexArray(VertexArrayObject);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
     // Check and call events and swap the buffers
     glfwPollEvents();
     glfwSwapBuffers(window);
   }
 
+  // De-allocate resources
+  glDeleteVertexArrays(1, &VertexArrayObject);
+  glDeleteBuffers(1, &VertexBufferObject);
+
+  // Terminate
   glfwTerminate();
 
   return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+  glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
 }
